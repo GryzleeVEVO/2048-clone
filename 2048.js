a0d4ed25a5e28f87942674ca35c0ba4599ec74a6
Dorian Wozniak <dorian.wozniak@gmail.com>
2024-11-19 15:29:03 +0100

    Moved game logic to class + tsconfig for static type checking

--- 2048.js
+++ 2048.js
@@ -1,3 +1,25 @@
+/**
+ * @file Implementation of a 2048 clone
+ * @author Dorian Wozniak <dorian.wozniak@gmail.com>
+ *
+ * This project implements a clone of the popular game 2048. The game consists
+ * of a board with empty and numbered tiles. The player must match numbers of
+ * the same value to add them up to 2048.
+ *
+ * The original game was created by Gabriele Cirulli, with itself being inspired
+ * by other similar games. You can play the original at https://play2048.co/.
+ */
+
+/* TODO
+  [ ] Add win state
+  [ ] Add fail state + restart button
+  [ ] Add board animations
+  [ ] Add scoreboard
+  [ ] Save state in local storage
+  [ ] A more elegant way to update board
+  [ ] Allow for boards with different sizes
+*/
+
 // Colors have been ripped from the classic version of 2048's CSS style
 const palette = new Map([
   ["2", "#EEE4DA"],
@@ -13,15 +35,14 @@ const palette = new Map([
   ["2048", "#edc22e"],
 ]);
 
-const size = 4;
-
-// REVIEW : Move the actual game logic to a separate file?
-// class Board {}
+const backgroundColor = "#bdac97";
+const textColor = "#756452";
+const textColorAlt = "#ffffff";
 
 /**
  * @param {number} min
  * @param {number} max
- * @returns {number}
+ * @returns {number} Integer between [min, max)
  */
 function getRandomInt(min, max) {
   // Stolen from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
@@ -30,233 +51,157 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-/**
- * @param {number} size
- */
-function pickAndSetRandomCell(size) {
-  let tiles = document.querySelectorAll(".game-tile");
-  let tile;
+class Board {
+  size = 4;
+  numTiles = this.size * this.size;
 
-  do {
-    tile = tiles[getRandomInt(0, size)];
-  } while (tile.innerHTML);
+  /// Chance for spawning a 4 instead of a 2. Larger = more improbable
+  #chanceForFour = 40;
 
-  // TODO: Chance as parameter
-  tile.innerHTML = getRandomInt(0, 15) === 0 ? "4" : "2";
-}
+  /**
+   * Encoded directions for update function
+   */
+  #dir = {
+    ArrowUp: {
+      x_start: this.size - 1,
+      y_start: 0,
+      x_step: -1,
+      y_step: 0,
+    },
+    ArrowDown: { x_start: 0, y_start: 0, x_step: 1, y_step: 0 },
+    ArrowLeft: {
+      x_start: 0,
+      y_start: this.size - 1,
+      x_step: 0,
+      y_step: -1,
+    },
+    ArrowRight: {
+      x_start: 0,
+      y_start: 0,
+      x_step: 0,
+      y_step: 1,
+    },
+  };
 
-function colorTiles() {
-  // TODO: Maybe use CSS classes for switching styles
-  for (let tile of document.querySelectorAll(".game-tile")) {
-    let tileNumber = tile.innerHTML;
+  /** @type {number[][]} */
+  data = Array.from({ length: this.size }, () => new Array(this.size).fill(0));
 
-    tile.style.backgroundColor =
-      tileNumber && palette.has(tileNumber)
-        ? palette.get(tileNumber)
-        : "#bdac97";
-    tile.style.color =
-      tileNumber && (tileNumber == "2" || tileNumber == "4")
-        ? "#756452"
-        : "#ffffff";
+  constructor() {
+    this.setRandomTile();
+    this.setRandomTile();
+  }
+
+  /**
+   * Update state of board after a move
+   * @param {string} key
+   */
+  update(key) {
+    let { x_start, y_start, x_step, y_step } = this.#dir[key];
+    let moved = false;
+
+    // TODO: Maybe find a way to reduce the number of checks required
+    for (let i = x_start; i >= 0 && i < this.size; i += x_step || 1) {
+      for (let j = y_start; j >= 0 && j < this.size; j += y_step || 1) {
+        // If empty, skip
+        if (this.data[i][j] === 0) continue;
+
+        // Find next empty tile
+        let next = { x: i, y: j };
+
+        while (
+          next.x + x_step >= 0 &&
+          next.x + x_step < this.size &&
+          next.y + y_step >= 0 &&
+          next.y + y_step < this.size &&
+          this.data[next.x + x_step][next.y + y_step] === 0
+        ) {
+          next.x += x_step;
+          next.y += y_step;
+        }
+
+        // Is next tile the same number? Then add them up
+        let pair = { x: next.x + x_step, y: next.y + y_step };
+
+        if (
+          pair.x >= 0 &&
+          pair.x < this.size &&
+          pair.y >= 0 &&
+          pair.y < this.size &&
+          this.data[pair.x][pair.y] === this.data[i][j]
+        ) {
+          this.data[pair.x][pair.y] += this.data[i][j];
+          this.data[i][j] = 0;
+          moved = true;
+          continue;
+        }
+
+        // Don't move if not necessary
+        if (next.x === i && next.y === j) continue;
+
+        // Else, just move the tile
+        this.data[next.x][next.y] = this.data[i][j];
+        this.data[i][j] = 0;
+        moved = true;
+      }
+    }
+
+    // Create new numbered tile
+    // TODO: Add check to see if the board is full in order to not spawn a new tile
+    if (moved) this.setRandomTile();
+    console.log(this.data);
+  }
+
+  /** Select a random empty tile and set its value to 2 or 4 */
+  setRandomTile() {
+    let i = 0;
+    let j = 0;
+
+    do {
+      i = getRandomInt(0, this.size);
+      j = getRandomInt(0, this.size);
+    } while (this.data[i][j] !== 0);
+
+    this.data[i][j] = getRandomInt(0, this.#chanceForFour) === 0 ? 4 : 2;
   }
 }
 
 /**
- * @param {Element} orig
- * @param {Element} dest
+ * @param {Board} board
  */
-function swapTiles(orig, dest) {
-  dest.innerHTML = orig.innerHTML;
-  orig.innerHTML = "";
-}
-
-/**
- * @param {Element} orig
- * @param {Element} dest
- */
-function addTiles(orig, dest) {
-  let total = (parseInt(dest.innerHTML) + parseInt(orig.innerHTML)).toString();
-  dest.innerHTML = total;
-  orig.innerHTML = "";
-}
-
-function upCheck() {
+function updateBoardView(board) {
   let tiles = document.querySelectorAll(".game-tile");
+  // @ts-ignore
+  console.assert(tiles, "Game board not in DOM!!!");
+  console.assert(
+    tiles.length === board.numTiles,
+    "Mismatch between number of DOM tiles and stored tiles"
+  );
 
-  for (let i = 1; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      let tile = tiles[j + size * i];
-      if (!tile.innerHTML) continue; // Square is empty
+  for (let i = 0; i < board.size; i++) {
+    for (let j = 0; j < board.size; j++) {
+      let tile = tiles[j + board.size * i];
+      let content = board.data[i][j] === 0 ? "" : board.data[i][j].toString();
 
-      // Find first empty square
-      let next_i = i;
-      let next_j = j;
+      // Update inner text
+      tile.innerHTML = content;
 
-      for (let i2 = i - 1; i2 >= 0; i2--) {
-        if (tiles[j + size * i2].innerHTML) break;
-        next_i = i2;
-      }
+      // Update color
+      // @ts-ignore
+      tile.style.backgroundColor =
+        content && palette.has(content)
+          ? palette.get(content)
+          : backgroundColor;
 
-      // Check if next square has a number
-      if (next_i - 1 >= 0) {
-        let nextTile = tiles[next_j + size * (next_i - 1)];
-
-        // If equal, add them up
-        if (nextTile.innerHTML === tile.innerHTML) {
-          addTiles(tile, nextTile);
-          continue;
-        }
-      }
-
-      // Don´t move if not necessary
-      if (next_i === i && next_j === j) continue;
-
-      swapTiles(tile, tiles[next_j + size * next_i]);
+      // @ts-ignore
+      tile.style.color =
+        content && (content == "2" || content == "4")
+          ? textColor
+          : textColorAlt;
     }
   }
 }
 
-function downCheck() {
-  let tiles = document.querySelectorAll(".game-tile");
-
-  for (let i = 2; i >= 0; i--) {
-    for (let j = 3; j >= 0; j--) {
-      let tile = tiles[j + size * i];
-      if (!tile.innerHTML) continue; // Square is empty
-
-      // Find first empty square
-      let next_i = i;
-      let next_j = j;
-
-      for (let i2 = i + 1; i2 < size; i2++) {
-        if (tiles[j + size * i2].innerHTML) break;
-        next_i = i2;
-      }
-
-      // Check if next square has a number
-      if (next_i + 1 < size) {
-        let nextTile = tiles[next_j + size * (next_i + 1)];
-
-        // If equal, add them up
-        if (nextTile.innerHTML === tile.innerHTML) {
-          addTiles(tile, nextTile);
-          continue;
-        }
-      }
-
-      // Don´t move if not necessary
-      if (next_i === i && next_j === j) continue;
-
-      swapTiles(tile, tiles[next_j + size * next_i]);
-    }
-  }
-}
-
-function leftCheck() {
-  let tiles = document.querySelectorAll(".game-tile");
-
-  for (let j = 1; j < size; j++) {
-    for (let i = 0; i < size; i++) {
-      let tile = tiles[j + size * i];
-      if (!tile.innerHTML) continue; // Square is empty
-
-      // Find first empty square
-      let next_i = i;
-      let next_j = j;
-
-      for (let j2 = j - 1; j2 >= 0; j2--) {
-        if (tiles[j2 + size * i].innerHTML) break;
-        next_j = j2;
-      }
-
-      // Check if next square has a number
-      if (next_j - 1 >= 0) {
-        let nextTile = tiles[next_j - 1 + size * next_i];
-
-        // If equal, add them up
-        if (nextTile.innerHTML === tile.innerHTML) {
-          addTiles(tile, nextTile);
-          continue;
-        }
-      }
-
-      // Don´t move if not necessary
-      if (next_i === i && next_j === j) continue;
-
-      swapTiles(tile, tiles[next_j + size * next_i]);
-    }
-  }
-}
-
-function rightCheck() {
-  let tiles = document.querySelectorAll(".game-tile");
-
-  for (let j = 2; j >= 0; j--) {
-    for (let i = 3; i >= 0; i--) {
-      let tile = tiles[j + size * i];
-      if (!tile.innerHTML) continue; // Square is empty
-
-      // Find first empty square
-      let next_i = i;
-      let next_j = j;
-
-      for (let j2 = j + 1; j2 < size; j2++) {
-        if (tiles[j2 + size * i].innerHTML) break;
-        next_j = j2;
-      }
-
-      // Check if next square has a number
-      if (next_j + 1 < size) {
-        let nextTile = tiles[next_j + 1 + size * next_i];
-
-        // If equal, add them up
-        if (nextTile.innerHTML === tile.innerHTML) {
-          addTiles(tile, nextTile);
-          continue;
-        }
-      }
-
-      // Don´t move if not necessary
-      if (next_i === i && next_j === j) continue;
-
-      swapTiles(tile, tiles[next_j + size * next_i]);
-    }
-  }
-}
-
-/**
- * @param {string} key
- */
-function update(key) {
-  // TODO: Now, make it work in all directions
-  // Each direction must be somewhat handled separately, since processing which
-  // tiles go where must be done in reverse direction
-  switch (key) {
-    case "ArrowUp":
-      upCheck();
-      break;
-    case "ArrowDown":
-      downCheck();
-      break;
-    case "ArrowLeft":
-      leftCheck();
-      break;
-    case "ArrowRight":
-      rightCheck();
-      break;
-  }
-
-  // Generate new tile
-  pickAndSetRandomCell(16);
-
-  // Color tiles accordingly
-  colorTiles();
-}
-
-/**
- * @param {KeyboardEvent} e
- */
-function keyDownHandler(e) {
+window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
     case "ArrowDown":
@@ -268,15 +213,9 @@ function keyDownHandler(e) {
       return;
   }
 
-  update(e.key);
-}
+  board.update(e.key);
+  updateBoardView(board);
+});
 
-function init() {
-  window.addEventListener("keydown", keyDownHandler);
-
-  pickAndSetRandomCell(16);
-  pickAndSetRandomCell(16);
-  colorTiles();
-}
-
-init();
+let board = new Board();
+updateBoardView(board);
